7a6de0ddd50480cf7195d21da5a1e615b2bc76f5
diotivano <diotivano@gmail.com>
2017-07-11 14:40:08 +0700

    perbaikan error

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php 
-<<<<<<< HEAD
 
-echo "test delete content index";
 
-?>
-=======
 include 'view/LoginUI.php';
 
 $lgn = new LoginUI();
 
 $lgn->aksesFormLogin();
 
-print_r(PDO::getAvailableDrivers());
- ?>
->>>>>>> dio
+print_r(PDO::getAvailableDrivers()); 
+
+?>
+
