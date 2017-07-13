519eb158b8293913d2756c1c482c3912bb02dbe0
diotivano <diotivano@gmail.com>
2017-07-13 14:48:36 +0700

    dio lihat list barang dan input barang dones

--- controller/BarangControl.php
+++ controller/BarangControl.php
@@ -16,8 +16,23 @@ class BarangControl extends Controller
 		$jenis = isset($_POST['jenis']) ? $_POST['jenis'] : "" ;
 		$pesan = $mbarang->inputBarang($nama_barang,$kode_barang,$stok,$harga,$jenis);
 		return $pesan;
+
 	}
 
+	public function editBarang()
+	{
+		include_once 'model/Barang.php';
+
+		/*$mbarang = new Barang();
+		$nama_barang = isset($_POST['nama_barang']) ? $_POST['nama_barang'] : "" ;
+		$kode_barang = isset($_POST['kode_barang']) ? $_POST['kode_barang'] : "" ;
+		$stok= isset($_POST['stok']) ? $_POST['stok'] : null ;
+		$harga = isset($_POST['harga']) ? $_POST['harga'] : 0 ;
+		$jenis = isset($_POST['jenis']) ? $_POST['jenis'] : "" ;
+		$mbarang->editBerita($nama_barang,$kode_barang,$stok,$harga,$jenis);
+		return $true;*/
+
+	}
 
 
 }
---
+++ editbarang.php
@@ -0,0 +1,8 @@
+<?php 
+include 'view/Karyawan.php';
+
+$edb = new EditBarangUI();
+
+$edb->editBarang();
+
+ ?>
